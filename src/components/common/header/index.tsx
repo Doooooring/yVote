818c04155a8f1ad613d345595ff2ee0d0bca59fb
kang seok <sekeipost@naver.com>
2023-01-07 18:32:24 +0900

    feat : make nav box component in header

--- src/components/common/header/index.tsx
+++ src/components/common/header/index.tsx
@@ -7,6 +7,38 @@ interface HeaderProps {
   setVacant: (vacant: boolean | NewsOrder) => void;
 }
 
+interface NavBoxProps {
+  link: String;
+  comment: String;
+  setVacant: (vacant: boolean | NewsOrder) => void;
+}
+
+const activeStyle = (isActive: boolean) => {
+  return isActive
+    ? {
+        borderBottom: '3px solid rgb(61, 152, 247)',
+        color: 'rgb(61, 152, 247)',
+      }
+    : {
+        borderBottom: 'none',
+        color: 'grey',
+      };
+};
+
+function NavBox({ link, comment, setVacant }: NavBoxProps) {
+  return (
+    <NavLink
+      to={`${link}`}
+      onClick={() => {
+        setVacant(true);
+      }}
+      style={({ isActive }) => activeStyle(isActive)}
+    >
+      {comment}
+    </NavLink>
+  );
+}
+
 export default function Header({ setVacant }: HeaderProps) {
   return (
     <Wrapper>
@@ -17,9 +49,21 @@ export default function Header({ setVacant }: HeaderProps) {
           </HomeLink>
         </LogoImgBox>
         <NavigationBox>
-          <NavLink to="/news">뉴스 모아보기</NavLink>
-          <NavLink to="/keywords">키워드 검색</NavLink>
-          <NavLink to="/analyze">정치 성향 테스트</NavLink>
+          <NavBox
+            link={'/news'}
+            comment="뉴스 모아보기"
+            setVacant={setVacant}
+          />
+          <NavBox
+            link={'/keywords'}
+            comment="키워드 검색"
+            setVacant={setVacant}
+          />
+          <NavBox
+            link={'/analyze'}
+            comment="정치 성향 테스트"
+            setVacant={setVacant}
+          />
         </NavigationBox>
       </HeaderBody>
     </Wrapper>
@@ -44,9 +88,32 @@ const Wrapper = styled.header`
   border-bottom-width: 4px;
 `;
 
-const HeaderBody = styled.div``;
+const HeaderBody = styled.div`
+  width: 1000px;
+  height: 100%;
+  position: relative;
+  margin-right: auto;
+  margin-left: auto;
+`;
 
-const LogoImgBox = styled.div``;
-const HomeLink = styled(NavLink)``;
+const LogoImgBox = styled.div`
+  position: absolute;
+  left: 0;
+  width: 30%;
+  height: 100%;
+`;
+const HomeLink = styled(NavLink)`
+  display: inline-flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100%;
+  text-decoration: none;
+`;
 const Logo = styled.img``;
-const NavigationBox = styled.div``;
+const NavigationBox = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
--- src/entities/interfaces/keywords/index.ts
+++ src/entities/interfaces/keywords/index.ts
@@ -1,7 +1,17 @@
+export enum category {
+  human = '인물',
+  politics = '정치',
+  policy = '정책 및 제도',
+  economy = '경제',
+  social = '사회',
+  organizatioin = '단체',
+  etc = '기타',
+}
+
 export interface Keyword {
   keyword: String;
   explain: String;
-  category: String;
+  category: category;
   recent: Boolean;
   news: Array<Number>;
 }
