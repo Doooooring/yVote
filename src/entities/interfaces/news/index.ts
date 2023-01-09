2da2e28577c95c821f201d6487783aa61019908c
kang seok <sekeipost@naver.com>
2023-01-09 16:19:48 +0900

    fix : configure the press type in six press name

--- src/entities/interfaces/news/index.ts
+++ src/entities/interfaces/news/index.ts
@@ -1,9 +1,11 @@
+type Press = '조선' | '중앙' | '동아' | '한겨례' | '한경' | '매경';
+
 export interface News {
   order: Number;
   title: String;
   summary: String;
   news: Array<{ date: Date; title: String; link: String }>;
-  journals: Array<{ press: String; title: String; link: String }>;
+  journals: Array<{ press: Press; title: String; link: String }>;
   keywords: Array<String>;
   state: Boolean;
   opinions: {
